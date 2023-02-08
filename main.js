fad8ca1c645f1092388be9ff69e925a1b28b82bd
ComfenalcoCursos2021 <educacioncontinua04@unc.edu.co>
2023-02-08 18:57:06 -0500

    Operadores Arigmeticos Libreria Matematica

--- main.js
+++ main.js
@@ -2,39 +2,34 @@ let num1 = 50, num2 = 32;
 let resultado;
 let myStyle = "background: #0c690c;color: #fff;font-family: sans-serif;padding: 5px;border: 2px solid #0c6963;border-radius: 5px;";
 
-//Operador de adicion 'Suma'
-resultado = num1 + num2;
-console.log(`%c${num1} + ${num2} = ${resultado}`, myStyle);
+// Math.ceil(x)
+// Devuelve el entero más pequeño mayor o igual que un número.
 
-//Operador de subtracion 'Resta'
-resultado = num1 - num2;
-console.log(`%c${num1} - ${num2} = ${resultado}`, myStyle);
+// Math.floor(x)
+// Devuelve el mayor entero menor que o igual a un número.
 
+// Math.max()
+// Devuelve el mayor de cero o más números.
 
-//Operador de Multiplicacion
-resultado = num1*num2;
-console.log(`%c${num1} X ${num2} = ${resultado}`, myStyle);
+// Math.min()
+// Devuelve el más pequeño de cero o más números.
 
-//Operador de Divsion
-resultado = num1/num2;
-console.log(`%c${num1} / ${num2} = ${resultado}`, myStyle);
+// Math.pow(x, y)
+// Las devoluciones de base a la potencia de exponente, que es, baseexponent.
 
-//Operador de Exponenciacion
-resultado = num1**num2;
-console.log(`%c${num1} ^ ${num2} = ${resultado}`, myStyle);
+// Math.random()
+// Devuelve un número pseudo-aleatorio entre 0 y 1.
 
-//Operador de Modulo o Residuo
-resultado = num1%num2;
-console.log(`%c${num1} % ${num2} = ${resultado}`, myStyle);
+// Math.round(x)
+// Devuelve el valor de un número redondeado al número entero más cercano.
 
+// Math.sqrt(x)
+// Devuelve la raíz cuadrada positiva de un número.
 
-//Operador de Unario Negativo
-num1 = -50;
-resultado = -num1;
-console.log(`%c - * - = -  ${num1} Negativo ${resultado}`, myStyle);
+// Math.trunc(x)
+// Devuelve la parte entera del número x, la eliminación de los dígitos fraccionarios.
 
-//Operador de Unario Positivo
-num2 = -32;
-resultado = +num2;
-console.log(`%c - * + = -  ${num2} Negativo ${resultado}`, myStyle);
+// 5.68545.toFixed(x)
+// Devuelve la cantidad de numeros decimal segun x
+
 
